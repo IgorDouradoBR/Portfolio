5ec48c4a0cfb4e0a21d95451d82edd0389239710
IgorDouradoBR <igor.d@edu.pucrs.br>
2024-12-01 15:36:03 -0300

    Fix: Move html5up-solid-state to public/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Nome do repositório
+  base: '/', // Raiz do domínio personalizado
 });
